1cc58057089640664dbaf46dfc46bcdd4c87ad2c
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-05-02 11:40:06 +0400

    Rewrite some functions

--- src/Base.hs
+++ src/Base.hs
@@ -13,13 +13,17 @@ identity x = x
 
 -- | Constant function.
 -- const :: a -> b -> a
-constant = \x y -> x
+-- constant = \x y -> x
+constant x y = x
 
 -- apply :: (t1 -> t2) -> t1 -> t2
-apply = \f x -> f x
+-- apply = \f x -> f x
+apply f x = f x
 
 -- compose :: (t1 -> t2) -> (t3 -> t1) -> t3 -> t2
-compose = \x y z -> x ( y (z))
+-- compose = \x y z -> x ( y (z))
+compose x y z = x ( y ( z ) )
 
 -- flip :: (a -> b -> c) -> b -> a -> c
-flip = \f x y -> f y x
+-- flip = \f x y -> f y x
+flip f x y = f y x
